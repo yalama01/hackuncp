548e3b4f450831646bf7a8d8cc09d55f1e945396
hackinmonster <eric@fackelman.net>
2025-03-23 07:08:26 -0400

    fixed func name

--- backend/external_logic/user_event.py
+++ backend/external_logic/user_event.py
@@ -1,5 +1,6 @@
 from backend.external_logic.helper import filter_dict
-from backend.internal_logic.find_people import rate_relevancy, find_people
+from backend.internal_logic.relevance_score import get_relevance_score
+from backend.internal_logic.find_people import find_people
 from backend.internal_logic.job_title import get_job_title_list
 from backend.internal_logic.bio_summary import make_bio
 from backend.internal_logic.email_draft import make_email
@@ -13,7 +14,7 @@ router = APIRouter()
 async def submit_event_location(event: ProjectSubmission):
     job_titles = get_job_title_list(event.project_overview)
     people = find_people(job_titles, location= event.location)
-    relevancy_scores = [rate_relevancy(person) for person in people]
+    relevancy_scores = [get_relevance_score(event.project_overview, person) for person in people]
     summarize_bios = [make_bio(person , event.project_overview ) for person in people]
     email = [make_email(person) for person in people]
 
